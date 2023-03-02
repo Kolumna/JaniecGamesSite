d5d1bab6ab7638fbd8da0307382b19961f6a949b
K0lumna <michaljaniec12@wp.pl>
2023-03-02 22:35:33 +0100

    fixes

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -7,9 +7,11 @@ const user = useUser();
     <section class="flex flex-col gap-16" v-if="user">
       <h1 class="text-6xl text-white font-bold">DO POBRANIA</h1>
       <section class="flex justify-center items-center py-12">
-        <button class="bg-green-400 text-7xl p-8 px-16 flex font-bold items-center justify-between gap-12">POBIERZ<font-awesome-icon
-              icon="fa-brands fa-windows"
-            /></button>
+        <button
+          class="bg-green-400 text-7xl p-8 px-16 flex font-bold items-center justify-between gap-12"
+        >
+          POBIERZ<font-awesome-icon icon="fa-brands fa-windows" />
+        </button>
       </section>
       <ul class="text-white text-xl">
         <li class="flex items-center gap-8 mb-4">
@@ -17,9 +19,7 @@ const user = useUser();
           <button
             class="bg-green-400 hover:bg-white transition-all duration-200 p-2 px-4 text-zinc-900 font-bold flex items-center gap-2"
           >
-            <i class="material-icons">download</i>POBIERZ<font-awesome-icon
-              icon="fa-brands fa-windows"
-            />
+            <i class="material-icons">download</i>POBIERZ
           </button>
         </li>
         <li class="flex items-center gap-8 mb-4">
@@ -27,9 +27,7 @@ const user = useUser();
           <button
             class="bg-green-400 hover:bg-white transition-all duration-200 p-2 px-4 text-zinc-900 font-bold flex items-center gap-2"
           >
-            <i class="material-icons">download</i>POBIERZ<font-awesome-icon
-              icon="fa-brands fa-windows"
-            />
+            <i class="material-icons">download</i>POBIERZ
           </button>
         </li>
         <li class="flex items-center gap-8 mb-4">
@@ -37,9 +35,7 @@ const user = useUser();
           <button
             class="bg-green-400 hover:bg-white transition-all duration-200 p-2 px-4 text-zinc-900 font-bold flex items-center gap-2"
           >
-            <i class="material-icons">download</i>POBIERZ<font-awesome-icon
-              icon="fa-brands fa-windows"
-            />
+            <i class="material-icons">download</i>POBIERZ
           </button>
         </li>
         <li class="flex items-center gap-8 mb-4">
@@ -47,9 +43,7 @@ const user = useUser();
           <button
             class="bg-green-400 hover:bg-white transition-all duration-200 p-2 px-4 text-zinc-900 font-bold flex items-center gap-2"
           >
-            <i class="material-icons">download</i>POBIERZ<font-awesome-icon
-              icon="fa-brands fa-windows"
-            />
+            <i class="material-icons">download</i>POBIERZ
           </button>
         </li>
       </ul>
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,10 @@ getPostsData().then((res) => (post.value = res));
     <section class="flex items-center justify-start w-full">
       <section class="w-full bg-white mr-4">
         <section
-          class="w-full h-full bg-green-400 translate-x-4 translate-y-4 pb-8"
+          class="w-full h-full bg-green-400 translate-x-4 translate-y-4 p-8"
         >
-          <h1 class="text-4xl lg:text-6xl w-full font-bold p-8">News</h1>
-          <p class="text-lg lg:text-2xl px-8">
+          <h1 class="text-4xl lg:text-6xl w-full font-bold mb-8">News</h1>
+          <p class="text-lg lg:text-2xl mb-8">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit
             quam id mollitia est numquam eos aspernatur suscipit, pariatur, a
             ratione sint laborum aperiam molestias asperiores quos deleniti
@@ -24,6 +24,11 @@ getPostsData().then((res) => (post.value = res));
             accusamus iste possimus eveniet suscipit ipsum consectetur earum
             necessitatibus cupiditate. Optio?
           </p>
+          <div class="flex justify-end">
+            <button class="p-4 px-8 bg-zinc-900 text-green-400 font-bold hover:bg-white transition-all duration-200">
+              ZOBACZ
+            </button>
+          </div>
         </section>
       </section>
     </section>
@@ -31,29 +36,26 @@ getPostsData().then((res) => (post.value = res));
       v-if="post"
       class="w-full flex flex-col items-center lg:items-end justify-between"
     >
-      <section class="bg-green-400 lg:h-[360px] w-full mr-4">
-        <section
-          class="bg-white flex justify-between flex-col lg:flex-row h-full translate-x-4 translate-y-4"
-        >
-          <section
-            class="bg-gray-200 w-full min-h-[280px] flex justify-center items-center"
+      <section class="bg-green-400 lg:h-[360px] w-full">
+        <NuxtLink :to="'/articles/' + post[0].id"
           >
-            <i class="material-icons scale-[400%]">photo</i>
+          <section
+            class="bg-white flex justify-between flex-col lg:flex-row h-full transition-all duration-200 hover:translate-x-4 hover:translate-y-4"
+          >
+            <section
+              class="bg-gray-200 w-full min-h-[280px] flex justify-center items-center"
+            >
+              <i class="material-icons scale-[400%]">photo</i>
+            </section>
+            <section class="w-full flex flex-col justify-between">
+              <h1 class="font-bold text-4xl p-8">{{ post[0].dane.title }}</h1>
+              <div class="flex gap-2 items-center p-8">
+                <div class="bg-green-400 w-4 h-4"></div>
+                <span class="font-bold">{{ post[0].dane.date }}</span>
+              </div>
+            </section>
           </section>
-          <section class="w-full flex flex-col justify-between">
-            <h1 class="font-bold text-4xl p-8">
-              <NuxtLink
-                class="hover:underline"
-                :to="'/articles/' + post[0].id"
-                >{{ post[0].dane.title }}</NuxtLink
-              >
-            </h1>
-            <div class="flex gap-2 items-center p-8">
-              <div class="bg-green-400 w-4 h-4"></div>
-              <span class="font-bold">{{ post[0].dane.date }}</span>
-            </div>
-          </section>
-        </section>
+        </NuxtLink>
       </section>
       <Btn class="mt-16 lg:mt-0" :text="`WIĘCEJ AKTUALNOŚCI`" to="/articles" />
     </section>
